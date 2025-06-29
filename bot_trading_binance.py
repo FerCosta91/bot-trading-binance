b4c21089ede651dab42d0671321111d093ade6a0
FerCosta91 <fernando1991costa@gmail.com>
2025-06-29 16:44:09 +0100

    Update bot_trading_binance.py

--- bot_trading_binance.py
+++ bot_trading_binance.py
@@ -13,7 +13,7 @@ client = Client(API_KEY, API_SECRET)
 print("🚀 Bot de trading iniciado com sucesso.")
 print("🌍 IP público da aplicação:", requests.get("https://api.ipify.org").text)
 
-symbols = ['BTCUSDT', 'ETHUSDT', 'LINKUSDT']
+symbols = ['SOLUSDT', 'AVAXUSDT', 'LINKUSDT']
 timeframe = '1h'
 short_ma = 9
 long_ma = 21
